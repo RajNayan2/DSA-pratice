9f7a40141899fdc7c4d548ae2cb4600bdacdcfc1
rajnayan209 <rajnayan2092004@gmail.com>
2026-08-14 00:16:01 +0530

    Add queue using linked list

---
+++ GENERAL_QUESTION/QUEUE/LinkedListQueue.java
@@ -0,0 +1,77 @@
+import java.util.*;
+public class LinkedListQueue{
+    public static class Node{
+        int val;
+        Node next;
+     Node(int val){
+        this.val=val;
+     }
+    }
+    public static class queueLL{
+        Node head=null;
+        Node tail=null;
+        int size=0;
+        public  void add(int x){
+            Node temp=new Node(x);
+            if(size==0){
+                head=tail=temp;
+            }else{
+                tail.next=temp;
+                tail=temp;
+            }
+            size++;
+        }
+        public int peek(){
+            if(size==0){
+                System.out.println("Queue is EMpty");
+                return -1;
+            }else{
+                return head.val;
+            }
+        }
+        public int remove(){
+            if(size==0){
+                System.out.println("Queue is EMpty");
+                return -1;
+            }else{
+                int x=head.val;
+                head=head.next;
+                size--;
+                return x;
+            }
+        }
+        public void  display(){
+            if(size==0){
+                System.out.println("Queue is EMpty");
+            }else{
+                Node temp=head;
+                while(temp!=null){
+                    System.out.print(temp.val+" ");
+                    temp=temp.next;
+                }
+            }
+            System.out.println();
+        }
+        public boolean isEmpty(){
+            if(size==0)return true;
+            else return false;
+        }
+    }
+        public static void main(String[] args) {
+        queueLL q1=new queueLL();
+        q1.display();
+        q1.add(5);
+        q1.add(4);
+        q1.add(3);
+        q1.add(8);
+        q1.add(7);
+        q1.display();
+        System.out.println(q1.peek());
+        q1.remove();
+        q1.display();
+       System.out.println(q1.isEmpty()); 
+       System.out.println(q1.size);
+
+    }
+
+}
